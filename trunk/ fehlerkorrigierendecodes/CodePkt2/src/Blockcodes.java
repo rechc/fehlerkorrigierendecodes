4351c2b90c3639778d2615a79830279592702d63
Rech.Christian <Rech.Christian@b9650798-d9c3-11de-bb05-635cec7d5d1b>
2009-11-25 13:13:59 +0000

    Fehlerkorrigierende Codes: Zweite Punkt Uebungsaufgabe. Uebungsblatt 3 --> Aufgabe 7

---
+++ trunk/ fehlerkorrigierendecodes/CodePkt2/src/Blockcodes.java
@@ -0,0 +1,31 @@
+/**
+ * Fehlerkorigierende Codes
+ * Wolfgang Braun
+ * 
+ * Uebungsblatt 3 - Aufgabe 7
+ * Zweite Punkt Aufgabe
+ */
+
+/**
+ * @author 
+ * @stand 25.11.09
+ * @version alpha 1.0
+ */
+public class Blockcodes {
+	
+	public int [][] blockcode;
+	
+	public void createBlockcodeMatrix(){
+		blockcode = new int [7][4];
+	}
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
